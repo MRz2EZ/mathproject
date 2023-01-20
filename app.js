6128fcdd7b880638b70c1238456d333b18c1802b
misterztoeasy <66268439+MRz2EZ@users.noreply.github.com>
2023-01-21 03:09:30 +0700

    calc()

--- app.js
+++ app.js
@@ -22,6 +22,64 @@ app.get('/', (req, res, next) => {
     res.status(300);
 });
 
+const rate_info = [
+    { name: "bbl", rate: {
+        qy: 0.375,
+        hy: 0.5,
+        fy: 0.5,
+    }},
+    { name: "ktb", rate: {
+        qy: 0.32,
+        hy: 0.4,
+        fy: 0.45,
+    }},
+    { name: "kbank", rate: {
+        qy: 0.32,
+        hy: 0.4,
+        fy: 0.4,
+    }},
+    { name : "scb", rate: {
+        qy: 0.32,
+        hy: 0.4,
+        fy: 0.4,
+    }},
+    { name: "gsb", rate: {
+        qy: 0.375,
+        hy: 0.5,
+        fy: 0.5,
+    }},
+    { name: "bay", rate: {
+        qy: 0.32,
+        hy: 0.4,
+        fy: 0.4,
+    }},
+];
+
+app.post('/', (req, res, next) => {
+    let info = {
+        bank: req.body.bank,
+        amount: Number(req.body.amount),
+        time: Number(req.body.time),
+    };
+    info.time = (info.time > 12) ? 12 : (info.time < 1) ? 1 : info.time;
+    info.amount = Math.abs(info.amount);
+    let __cur__rate = 0;
+    for(let i = 0; i<rate_info.length; i++) {
+        if(info.bank === rate_info[i].name) {
+            if(info.time <= 3) {
+                __cur__rate = rate_info[i].rate.qy;
+            } else if(info.time <= 6) {
+                __cur__rate = rate_info[i].rate.hy;
+            } else __cur__rate = rate_info[i].rate.fy;
+        };
+    };
+    let interest_cal = Math.pow(1 + (__cur__rate/100), 0.0833333 * info.time);
+    interest_cal = Number(interest_cal).toFixed(6);
+    let mon_acc = Number(Number(info.amount) * Number(interest_cal));
+    res.json({bank: info.bank, rate: __cur__rate, amount: info.amount, time: info.time, interest_recieved: Number(Number(mon_acc) - Number(info.amount)).toFixed(6), money_recieved: mon_acc.toFixed(6)});
+    res.status(300);
+});
+
 app.use((req, res, next) => {
     res.send("Not Found!");
     res.status(404);
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,38 @@
 let bank_obj = document.getElementsByName('bank-slct');
 let img_obj = document.getElementsByName('img-slct');
+let __cur = 'kbank';
 for(let i = 0; i<img_obj.length; i++) {
     img_obj[i].addEventListener('click', () => {
         bank_obj[i].checked = true;
+        __cur = bank_obj[i].value;
     });
 };
+let info_obj = document.getElementById('info');
+info_obj.addEventListener('click', () => {
+    Swal.fire({
+        icon: 'info',
+        html: `คณิตศาสตร์กับการเงินในชีวิตประจำวัน (ดอกเบี้ยทบต้น)<br>จัดทำโดย<br>นายปรมินทร์ ช่วยพิชัย<br>นางสาวกชพรรณ วงศ์น้อย<br>นางสาวภัทรนันท์ หนูแก้ว<br>ชั้นมัธยมศึกษาปีที่ 5 โรงเรียนสุราษฎร์ธานี<br><span style="font-weight:bolder;">อัตราดอกเบี้ยเป็นข้อมูลจากธนาคารแห่งประเทศไทย ณ วันที่ 4 มกราคม 2565</span>`
+    });
+});
+
+let input_amount = document.getElementById('amount');
+let input_time = document.getElementById('time');
 
 function calc() {
-    Swal.fire('test');
+    if(input_amount.value === "" || input_time.value === "" || __cur === "") {
+        Swal.fire({
+            icon: 'info',
+            text: 'Please specify all of information',
+        });
+    } else if(isNaN(input_amount.value) == true || isNaN(input_time.value) == true || parseInt(Number(input_amount.value)) != input_amount.value || parseInt(Number(input_time.value) != input_time.value)) {
+        Swal.fire({icon: 'info', text: 'All information must be an positive integer value.'});
+    } else if(input_time.value > 12 || input_time.value < 1) {
+        Swal.fire({icon: 'info', text: 'Time must be between 1 to 12'});
+    } else {
+        $.post(window.location.href, {bank: __cur, amount: input_amount.value, time: input_time.value}, (error) => {}).done((d) => {
+            document.getElementById('interest_rate').value = `${d.rate}%`;
+            document.getElementById('interest_received').value = `${d.interest_recieved} Baht`;
+            document.getElementById('money_received').value = `${d.money_recieved} Baht`;
+        });
+    };
 };
